c91ab63b3bc0797cdd1402cfec413d84fdba33c3
Eric Strohmaier <eric@Sigmund.local>
2023-10-24 09:51:49 -0400

    startign project added pico css

---
+++ components/Searchbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <form>
+    <div class="grid">
+      <input
+        type="text"
+        name="firstname"
+        placeholder="First name"
+        aria-label="First name"
+        required
+      />
+      <input
+        type="email"
+        name="email"
+        placeholder="Email address"
+        aria-label="Email address"
+        required
+      />
+      <button type="submit">Subscribe</button>
+    </div>
+    <fieldset>
+      <label for="terms">
+        <input type="checkbox" role="switch" id="terms" name="terms" />
+        I agree to the
+        <a href="#" onclick="event.preventDefault()">Privacy Policy</a>
+      </label>
+    </fieldset>
+  </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Index Page</h1>
+ 
+  <section  class="section">
+      <h1 class="title">Welcome to Our Landing Page</h1>
+      <p class="description">A simple and clean landing page created with Pico CSS.</p>
+    </section>
+
+
+  </div>
+</template>
+
+
+<script>
+
+export default {
+  name: 'IndexPage',
+}
+</script>
