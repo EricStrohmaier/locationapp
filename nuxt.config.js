6d3e5ca1a524b517583a26d41cbbfbff49d63d78
Eric Strohmaier <eric@Sigmund.local>
2023-10-25 06:03:02 -0400

    vercel

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,47 +1,46 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+  target: "static",
+  ssr: false,
+  generate: {
+    fallback: false,
+  },
   head: {
-    title: 'locationapp',
+    title: "locationapp",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
   router: {
     extendRoutes(routes) {
       routes.push({
-        path: '/location/:locationName',
-        component: 'pages/location/_index.vue',
+        path: "/location/:locationName",
+        component: "pages/location/_index.vue",
       });
     },
   },
 
-  css: ['~/assets/css/pico.min.css'], // Correct the CSS reference
+  css: ["~/assets/css/pico.min.css"], // Correct the CSS reference
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {},
+};
